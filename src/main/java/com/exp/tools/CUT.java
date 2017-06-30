e1ee13163f779401d437bb3de6e8a24b8e0ec37d
arpit.gautam <arpit.gautam@kronos.com>
2017-06-30 17:41:35 +0530

    adding project to be instrumented by jacoco

---
+++ src/main/java/com/exp/tools/CUT.java
@@ -0,0 +1,9 @@
+package com.exp.tools;
+
+public class CUT {
+	
+	public void meth(){
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/test/java/com/exp/tools/CUTest.java
@@ -0,0 +1,15 @@
+package com.exp.tools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CUTest {
+
+	@Test
+	public void test() {
+		CUT c = new CUT();
+		c.meth();
+	}
+
+}
